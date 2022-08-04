29aa1c2ae001fea37389a17691c9765313a29604
Nicholas <91276870+NEOCLOUD0@users.noreply.github.com>
2022-08-04 13:42:31 -0400

    Delete Exception Handling Lessons directory

--- Exception Handling Lessons/immutable/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person person = new Person("Ryan", "Canadian"); // Mutable or Immutable?
-        Person person2 = new Person("John", "America");											// Immutable because no setters
-        System.out.println(person);
-        System.out.println(person2);
-        
-    }
-}
--- Exception Handling Lessons/unchecked-exceptions/RuntimeException1.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class RuntimeException1 {
-    public static void main(String[] args) {
-        int[] array = new int[3];
-        System.out.println(array[2]);
-
-    }
-}
