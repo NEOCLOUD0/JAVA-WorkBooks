29734a8f851920627bc1a5cf264d463dbac77bf9
Nicholas <91276870+NEOCLOUD0@users.noreply.github.com>
2022-12-03 12:48:46 -0500

    Add files via upload

---
+++ 1. Practice/other/Multiplication table/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int stockNum = 5;
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(stockNum + "*" + i + ":" + (stockNum * i));
+        }
+    }
+}
